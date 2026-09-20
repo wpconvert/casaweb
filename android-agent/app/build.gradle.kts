0f8ae061c42c25075662ef136cbfda1cc40b6e92
convert <danip4848@gmail.com>
2026-09-20 13:08:10 +0700

    Fix Android Kotlin build configuration

--- android-agent/app/build.gradle.kts
+++ android-agent/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -28,10 +27,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
