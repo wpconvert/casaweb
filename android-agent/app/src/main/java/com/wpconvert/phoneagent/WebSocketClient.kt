dd053915e104ce1edd0b986c75198eeafe9116d2
convert <danip4848@gmail.com>
2026-09-20 20:54:14 +0700

    WebSocketClient.kt

--- android-agent/app/src/main/java/com/wpconvert/phoneagent/WebSocketClient.kt
+++ android-agent/app/src/main/java/com/wpconvert/phoneagent/WebSocketClient.kt
@@ -25,7 +25,7 @@ class WebSocketClientManager(
 
 
         // GANTI DENGAN IP PC SERVER
-        val serverUrl = "ws://192.168.105.250:8765"
+        val serverUrl = "wss://web-phone-oneforall.danip4848.workers.dev/ws"
 
 
         socket = object : WebSocketClient(
